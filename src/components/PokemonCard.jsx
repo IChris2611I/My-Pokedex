0f09f3e13446d2e40b11140f6eda49c6052fe61e
IChris2611I <pachris2611@gmail.com>
2024-04-08 10:55:10 +0200

    PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PokemonCard = () => {
+  return (
+    <div>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Pokemon"
+        />
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    </div>
+  );
+};
+export default PokemonCard;
